3f51296e474eb4e1245d85072259b5730add247a
Jon Bell <github@lot23.com>
2022-10-25 23:00:05 +1300

    Auto-commit (bookshelf)

--- cypress/e2e/1-helloworld.cy.js
+++ cypress/e2e/1-helloworld.cy.js
@@ -1,8 +1,8 @@
 describe("Check some stuff", () => {
-  it("passes", () => {
+  it("Loads good website", () => {
     cy.visit("https://bookshelf.lot23.com");
   });
-  it("passes", () => {
-    cy.visit("https://bookshelf.lot23.com");
+  it("Loads bad website", () => {
+    cy.visit("https://bookshelf.lot25.com");
   });
 });
